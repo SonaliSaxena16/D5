ab765369a01ac61ed87dfcf489e9c895d60f8035
sonali016 <saxenasonali094@gmail.com>
2024-01-12 00:49:13 +0530

    recent

---
+++ AllAboutJava/JavaBasics/src/constructorBasics/ConstShowsDefaultValues.java
@@ -0,0 +1,22 @@
+package constructorBasics;
+
+public class ConstShowsDefaultValues {
+
+	int id;  
+	String name;  
+	//method to display the value of id and name  
+	void display(){System.out.println(id+" "+name);} 
+	void show() {System.out.println(id); }
+	  
+	public static void main(String args[]){  
+	//creating objects  
+		ConstShowsDefaultValues s1=new ConstShowsDefaultValues();  
+		//ConstShowsDefaultValues s2=new ConstShowsDefaultValues();  
+	//displaying values of the object  
+	s1.display(); 
+	s1.show();
+	//s2.display();  
+	}  
+	}  
+	
+	
