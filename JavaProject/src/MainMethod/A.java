6be953d9fd77f6d0de1e3c9f5fe8a64cec6ae970
Sonali Saxena <sonalisaxenain@gmail.com>
2024-12-25 22:40:05 +0530

    Changes

--- JavaProject/src/MainMethod/A.java
+++ JavaProject/src/MainMethod/A.java
@@ -1,13 +1,12 @@
-package allAboutMainMethod;
+package MainMethod;
 
-public class B {
-	
+public class A {
+
 //FAMOUS INTERVIEW QUE- Can we call main method from different class ? Yes shown below..
 	//Output will go in loop for infinite times at last throws error due to memomy full
-//Controlled shifting from A to B and B to A
 	public static void main(String[] args) {
-		System.out.println("B class main method");
-		A.main(args);
+		System.out.println("A class main method");
+		B.main(args);
 	}
 
 }
---
+++ JavaProject/src/MainMethod/mainMethodOverloading.java
@@ -0,0 +1,40 @@
+package MainMethod;
+
+// Interview Question1 - Can main() be OVERLOADED ?
+// Ans - Yes however JVM will try to search for exact method signture i.e.(public static void main(String[] args))
+// JVM does not know any other main method if not found String[] args these parametrs JVM will not execute ur code
+
+// Interview Question2 - Can main() be OVERRIDDEN ?
+// Ans - No, we cannot override main method of java because a static method cannot be overridden.
+// Bcz static method in java is associated with class and object creation isn't required 
+// whereas the non-static method is associated with an object.
+
+public class mainMethodOverloading {
+
+	public static void main(String args){
+		System.out.println("main method 2");
+	}
+	
+	public static void main(int a){
+		System.out.println("main method 3");
+	}
+	
+	public static void main(int a, int b){
+		System.out.println("main method 4");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("main method 1");
+		
+		main("Sonali");
+		main(10);
+		main(40,50);
+		
+		// Output 
+	//	main method 1
+	// main method 2
+		// main method 3
+		// main method 4
+	}
+	
+}
