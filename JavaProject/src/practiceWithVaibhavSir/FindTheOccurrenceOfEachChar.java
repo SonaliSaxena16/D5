8d6056ff65eaeb96105d898572ba2ba8e4d92503
Sonali Saxena <sonalisaxenain@gmail.com>
2025-03-04 22:54:53 +0530

    Code

--- JavaProject/src/practiceWithVaibhavSir/FindTheOccurrenceOfEachChar.java
+++ JavaProject/src/practiceWithVaibhavSir/FindTheOccurrenceOfEachChar.java
@@ -4,9 +4,8 @@ public class FindTheOccurrenceOfEachChar {
 
 	public static void main(String[] args) {
 
-		int count=1;
-		
 		char ch[] = {'s','s','o','o','n','n','a','a','l','l','i','i'};
+		int count=1;
 		
 		for(int i=0; i<ch.length; i++) {
 			
@@ -15,7 +14,7 @@ public class FindTheOccurrenceOfEachChar {
 			}
 			
 			else {
-				System.out.println(ch[i] + count);
+				System.out.println(ch[i] + ":" + count);
 				count=1;
 			}
 		}
---
+++ JavaProject/src/practiceWithVaibhavSir/PrintNameInStarPattern.java
@@ -0,0 +1,28 @@
+package practiceWithVaibhavSir;
+
+public class PrintNameInStarPattern {
+
+	public static void main(String[] args) {
+		
+		//s
+		//so
+		//son
+		//sona
+		//sonal
+		//sonali
+		
+		char ch[] = {'s','o','n','a','l','i'};
+		
+		for(int i=0; i<ch.length; i++) {
+			
+			for(int j=0; j<=i; j++) {
+				System.out.print(ch[j]);
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
+
+
